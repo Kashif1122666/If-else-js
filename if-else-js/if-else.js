37433da99a349aaa954552f341e555d51f5f75cc
Kashif1122666 <158280129+Kashif1122666@users.noreply.github.com>
2024-07-26 13:51:35 -0700

    Add files via upload
    if-else-js

---
+++ if-else-js/if-else.js
@@ -0,0 +1,56 @@
+// CHAPTER-12 IF ELSE STATMENT
+
+// program about rain expectation
+// const cloud = true;
+
+// if it's cloudy today
+// if (cloud) {
+//    alert("it's gonna be raining soon in peshawar");
+// }
+
+// // if it's not cloudy today
+// if (!cloud) {
+//     alert("no chance of rain in peshawar");
+// }
+
+// using if-else
+// if (cloud) {
+//   alert("it's gonna be raining soon in peshawar");
+// } else {
+//     alert("no chance of rain in peshawar");
+// }
+
+// real world example
+// const hunger = true; 
+// const money = true;
+
+// if (hunger && money) {
+//    alert("Get a burger");
+// } else {
+//     alert("chill let's go without burger");
+// }
+
+// question : negate value or variable
+
+// negating value, allowed
+// const cloud = !false;
+// console.log(badal);
+
+// negating variable, allowed
+// const cloud = false;
+// console.log(!badal);
+
+// negating variable right at the time of declaration, not allowed
+// const !cloud = false;
+// console.log(badal);
+
+// // Program: EVEN/ODD
+// const username = prompt("please enter your name");
+// const userinput = +prompt("please enter any number");
+
+// const remainder = userinput % 2;
+// if (remainder === 0) {
+//    alert(username + " brother you have entered an even number");
+// } else {
+//     alert(`${username} brother you have entered an odd number`);
+}
